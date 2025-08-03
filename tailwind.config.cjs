75e53ebb6cc281c3fd72def50df6270916cfc6aa
Garik Ghazaryan <garikgh2009@gmail.com>
2025-08-03 17:20:31 +0400

    Remove Tailwind Media in CSS file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,8 @@
 module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,jsx,ts,tsx}"
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx,css}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
 };
