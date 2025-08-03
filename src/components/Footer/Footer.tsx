3c0e1af42171dc477c235d600cb29d9e56601bdd
Garik Ghazaryan <garikgh2009@gmail.com>
2025-08-03 19:13:48 +0400

    footer.jsx convert to .tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,6 @@
-export default function Footer() {
+import React from "react";
+
+export default function Footer(): React.ReactElement {
   return (
     <div className="finalFooter">
       <div className="footerGlass">
