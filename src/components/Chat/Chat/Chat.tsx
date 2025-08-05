93634c403e56b47c15a2ba452a5f9121a0eca40d
Garik Ghazaryan <garikgh2009@gmail.com>
2025-08-05 16:19:50 +0400

    debugging code

--- src/components/Chat/Chat/Chat.tsx
+++ src/components/Chat/Chat/Chat.tsx
@@ -66,7 +66,7 @@ const Chat: React.FC = () => {
       });
       setNewMessage("");
     } catch (error) {
-      console.error("Error sending message:", error);
+      console.log(error);
     }
   };
 
