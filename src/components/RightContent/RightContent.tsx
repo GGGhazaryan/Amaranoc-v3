40a507910dc1e224ba9360bb0c3eb32f4b25301f
Garik <garikgh2009@gmail.com>
2025-08-19 13:25:24 +0400

    Added items for a header

--- src/components/RightContent/RightContent.tsx
+++ src/components/RightContent/RightContent.tsx
@@ -6,12 +6,12 @@ export default function RightContent(): React.ReactElement {
   return (
     <main className="rightContentMain" style={{ marginTop: "5%" }}>
       <div className="headersContainer">
-        <div className="rightContentHeader"><img src="./public/item1.png" alt="item1" /></div>
-        <div className="rightContentHeader"><img src="./public/item2.png" alt="item2" /></div>
-        <div className="rightContentHeader"><img src="./public/item3.png" alt="item3" /></div>
-        <div className="rightContentHeader"><img src="./public/item4.png" alt="item4" /></div>
-        <div className="rightContentHeader"><img src="./public/item5.png" alt="item5" /></div>
-        <div className="rightContentHeader"><img src="./public/item6.png" alt="item6" /></div>
+        <div className="rightContentHeader"><img className="itemsInHeader" src="./public/item1.png" alt="item1" /></div>
+        <div className="rightContentHeader"><img className="itemsInHeader" src="./public/item2.png" alt="item2" /></div>
+        <div className="rightContentHeader"><img className="itemsInHeader" src="./public/item3.png" alt="item3" /></div>
+        <div className="rightContentHeader"><img className="itemsInHeader" src="./public/item4.png" alt="item4" /></div>
+        <div className="rightContentHeader"><img className="itemsInHeader" src="./public/item5.png" alt="item5" /></div>
+        <div className="rightContentHeader"><img className="itemsInHeader" src="./public/item6.png" alt="item6" /></div>
       </div>
       <div className="rightContentWrapper">
         {cards.map(card => (  
