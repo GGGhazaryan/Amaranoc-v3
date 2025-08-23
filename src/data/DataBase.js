0b8f939dd606554b5b1f5c835bcb741ba6ffe53c
Garik <garikgh2009@gmail.com>
2025-08-24 01:44:57 +0400

    deleted dubled cards in DB

--- src/data/DataBase.js
+++ src/data/DataBase.js
@@ -3,22 +3,22 @@ const cards = [
   { id: 2, image: 'firstphoto.webp', title: 'Ձորաղբյուր', price: ' 115,000 ֏', location: 'Ձորաղբյուր', people: '4' },
   { id: 3, image: '3photo.webp', title: 'Ծաղկաձոր', price: ' 125,000 ֏', location: 'Ծաղկաձոր', people: '8' },
   { id: 4, image: '4photo.webp', title: 'Երևան', price: '118,000 ֏', location: 'Երևան', people: '5' },
-  { id: 5, image: '5photo.webp', title: 'Աշտարակ', price: ' 112,000 ֏', location: 'Աշտարակ', people: '3' },
+  { id: 5, image: '5photo.webp', title: 'Երևան', price: ' 112,000 ֏', location: 'Երևան', people: '3' },
   { id: 6, image: '6photo.webp', title: 'Դիլիջան', price: ' 130,000 ֏', location: 'Դիլիջան', people: '10' },
   { id: 7, image: '7photo.webp', title: 'Գյումրի', price: ' 122,000 ֏', location: 'Գյումրի', people: '6' },
-  { id: 8, image: '8photo.webp', title: 'Սևան', price: ' 117,000 ֏', location: 'Սևան', people: '4' },
+  { id: 8, image: '8photo.webp', title: 'Ծաղկաձոր', price: ' 117,000 ֏', location: 'Սևան', people: '4' },
   { id: 9, image: '9photo.webp', title: 'Վանաձոր', price: ' 135,000 ֏', location: 'Վանա լիճ', people: '8' },
-  { id: 10, image: '10photo.webp', title: 'Ճամբարակ', price: '128,000 ֏', location: 'Ճամբարակ', people: '7' },
+  { id: 10, image: '10photo.webp', title: 'Ծաղկաձոր', price: '128,000 ֏', location: 'Ծաղկաձոր', people: '7' },
   { id: 11, image: '11photo.webp', title: 'Երևան', price: ' 121,000 ֏', location: 'Երևան', people: '5' },
   { id: 12, image: '12photo.webp', title: 'Հրազդան', price: ' 119,000 ֏', location: 'Հրազդան', people: '4' },
   { id: 13, image: '13photo.webp', title: 'Սևան', price: ' 124,000 ֏', location: 'Սևան', people: '6' },
   { id: 14, image: '14photo.webp', title: 'Աշտարակ', price: ' 114,000 ֏', location: 'Աշտարակ', people: '2' },
   { id: 15, image: '15photo.webp', title: 'Երևան', price: ' 110,000 ֏', location: 'Երևան', people: '4' },
   { id: 16, image: '5photo.webp', title: 'Աշտարակ', price: ' 112,000 ֏', location: 'Աշտարակ', people: '3' },
-  { id: 17, image: '6photo.webp', title: 'Դիլիջան', price: ' 130,000 ֏', location: 'Դիլիջան', people: '10' },
-  { id: 18, image: 'firstphoto.webp', title: 'Ձորաղբյուր', price: ' 115,000 ֏', location: 'Ձորաղբյուր', people: '4' },
-  { id: 19, image: '3photo.webp', title: 'Ծաղկաձոր', price: ' 125,000 ֏', location: 'Ծաղկաձոր', people: '8' },
-  { id: 20, image: '4photo.webp', title: 'Երևան', price: ' 118,000 ֏', location: 'Երևան', people: '5' }
+  { id: 17, image: '6photo.webp', title: 'Աշտարակ', price: ' 130,000 ֏', location: 'Աշտարակ', people: '10' },
+  { id: 18, image: 'firstphoto.webp', title: 'Երևան', price: ' 115,000 ֏', location: 'Երևան', people: '4' },
+  { id: 19, image: '3photo.webp', title: 'Ճամբարակ', price: ' 125,000 ֏', location: 'Ճամբարակ', people: '8' },
+  { id: 20, image: '4photo.webp', title: 'Ձորաղբյուր', price: ' 118,000 ֏', location: 'Ձորաղբյուր', people: '5' }
 ];
 
 export default cards;
